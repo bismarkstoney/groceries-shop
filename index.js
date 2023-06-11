093653783fe8218fe3fa87fef43a66ed3411dcc7
Bismark Obeng <bismarkstoney@yahoo.com>
2023-06-11 16:24:41 +0000

    google auth

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 import  dotenv from 'dotenv'
 dotenv.config()
 import express from 'express'
-import session from "express-session"
-import  passport  from "passport";
+import  GOOGLESTRATEGY  from 'passport-google-oauth20'
+import passport from 'passport'
 import productRouter from './routes/productRouter.js'
 import categoryRouter from './routes/categoryRoute.js'
 import connecteDB from './config/db.js'
@@ -12,73 +12,21 @@ const PORT=  process.env.PORT || 3000
 connecteDB()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
-passport.serializeUser((user, done) => {
-    done(null, user.id);
-  });
-  passport.deserializeUser((id, done) => {
-    User.findById(id, (err, user) => {
-      done(err, user);
-    });
-  });
-  app.use(
-    session({
-      secret: process.env.SESSION_SECRET,
-      resave: false,
-      saveUninitialized: false,
-    })
-  );
-app.use(passport.initialize());
-app.use(passport.session());
+
+
+
 app.use('/api/v1/product', productRouter)
 app.use('/api/v1/category',categoryRouter)
-app.get("/", (req, res) => {
-    if (req.isAuthenticated()) {
-      res.send(`Hello ${req.user.username}! <a href="/logout">Logout</a>`);
-    } else {
-      res.send(`Hello Guest! <a href="/login">Login</a>`);
-    }
-  });
-  
-  app.get("/login", (req, res) => {
-    res.send(`
-      <form action="/login" method="post">
-        <div>
-          <label>Username:</label>
-          <input type="text" name="username" />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input type="password" name="password" />
-        </div>
-        <div>
-          <input type="submit" value="Login" />
-        </div>
-      </form>
-      <a href="/register">Register</a>
-    `);
-  });
-  
-  app.post(
-    "/login",
-    passport.authenticate("local", {
-      successRedirect: "/",
-      failureRedirect: "/login",
-    })
-  );
-  
-  app.get("/auth/google", passport.authenticate("google", { scope: ["profile", "email"] }));
-  
-  app.get(
-    "/auth/google/callback",
-    passport.authenticate("google", { failureRedirect: "/login" }),
-    (req, res) => {
-      res.redirect("/");
-    }
-  );
-  
-  app.get("/logout", (req, res) => {
-    req.logout();
-    res.redirect("/");
-  });
+passport.use (new GOOGLESTRATEGY({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL : '/auth/google/callback'
+}, (accessToken, refreshToken, profile, done)=>{
+    console.log(accessToken,profile )
+}))
+app.get('/auth/api/v1/google', passport.authenticate('google',{
+    scope: ['profile', 'email']
+}))
 
+app.get('/auth/google/callback', passport.authenticate('google'))
 app.listen(PORT, console.log(`The server is running in ${process.env.NODE_ENV} on  ${PORT}`))
